4710e7585b8cefc785fb873ca329223ad09ef700
Roman Gusev <groman.me@gmail.com>
2013-07-10 23:43:57 +0400

    wip: add errors and show error pages

--- lib/airbrake_explorer/application.rb
+++ lib/airbrake_explorer/application.rb
@@ -1,11 +1,20 @@
 require 'sinatra/base'
+require 'airbrake-api'
 
 module AirbrakeExplorer
   class Application < Sinatra::Base
     set :root, ROOT_PATH
 
     get '/' do
-      haml :index
+      haml :index, locals: { ab_errors: api.errors }
+    end
+
+    get '/errors/:error_id' do
+      haml :error, locals: { error: api.error(params['error_id'].to_i)}
+    end
+
+    def api
+      @api ||= AirbrakeAPI::Client.new AirbrakeExplorer::Config.instance.ab_credentials
     end
   end
 end
