2ed2707a67cba322e42d70e4af2e6dea71088d07
Roman Gusev <groman.me@gmail.com>
2013-11-10 22:28:43 +0400

    fix formatting for pretty tag

--- lib/sinatra/application_helper.rb
+++ lib/sinatra/application_helper.rb
@@ -6,14 +6,14 @@ module Sinatra
       params['error_id'].to_i
     end
 
-    def pretty_pre_tag(obj)
+    def pretty_tag(obj)
       text = ''
       PP.pp obj, text
-      "<pre><code>#{text}</code></pre>"
+      text
     end
 
     def error_link(error)
-      haml_tag :a , {href: "#{current_project}/errors/#{error.id}"} do
+      haml_tag :a , {href: "errors/#{error.id}"} do
         haml_concat error.error_class
       end
     end
